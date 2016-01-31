44a3b57f4aea7ef597c36f9cd7f0e79778f079cd
Peter Carrero <peter.carrero@gmail.com>
2016-01-30 19:33:21 -0600

    Add more description to examples files

--- config-examples.sh
+++ config-examples.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 
 # this file has example settings that should work as default values for most installations
+# you should copy this file into config.sh and then adjust config.sh to fit your environment
 acme_tiny_local_folder='/opt/acme-tiny'
