ff0481e52fb5b175c57c8387a78cbd0dffd9c2c8
Peter Carrero <peter.carrero@gmail.com>
2016-01-30 20:10:30 -0600

    Add parameter check, tools checks

--- acme-tiny-automator.sh
+++ acme-tiny-automator.sh
@@ -6,13 +6,42 @@
 # and adjust accordingly
 . ./config.sh
 
+function usage() {
+  >&2 echo "Usage: $0 <domain-name-to-create>"
+  exit 1
+}
+
+function check_component() {
+    RESULT=$(which $1)
+    if [ $? -gt 0 ]; then
+        >&2 echo "Missing $1"
+        >&2 echo "Without it, this script won't work correctly"
+        >&2 echo "Please fix this error and try again"
+        exit 1
+    else
+        echo $RESULT
+    fi
+}
+
+# check parameters
+if [ $# -ne 1 ] || [ $1 == '-h' ]; then
+    usage
+fi
+
 # general settings
 ACME_TINY_URL='https://raw.githubusercontent.com/diafygi/acme-tiny/master/acme_tiny.py'
 LETSENCRYPT_INTERMEDIATE_URL='https://letsencrypt.org/certs/lets-encrypt-x1-cross-signed.pem'
 
 # check for common tools
-GIT=$(which git)
-PYTHON=$(which python)
+ACME_TINY="$ACME_TINY_LOCAL_FOLDER/acme-tiny.py"
+GIT=$(check_component git)
+PYTHON=$(check_component python)
+WGET=$(check_component wget)
 
-# check for python libraries
+# download acme-tiny, if it doesnt exist
+if [ ! -d "$ACME_TINY_LOCAL_FOLDER" ]; then
+    mkdir -p $ACME_TINY_LOCAL_FOLDER
+    $WGET $ACME_TINY_URL -O $ACME_TINY
+fi
+
 
