8dbdc606958bc3aacc1963090f6cfed17d98b342
Peter Carrero <peter.carrero@gmail.com>
2016-01-30 20:42:53 -0600

    Delete config.sh

--- config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# this file has example settings that should work as default values for most installations
-# you should copy this file into config.sh and then adjust config.sh to fit your environment
-ACME_TINY_LOCAL_FOLDER='/opt/acme-tiny'
-LETSENCRYPT_ROOT='/etc/httpd/ssl/letsencrypt'
-APACHE_ROOT='/var/www'
