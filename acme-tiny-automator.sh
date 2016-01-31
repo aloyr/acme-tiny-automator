4f6aaff1725e6cd02b4ef7307fc07824409b2acd
Peter Carrero <peter.carrero@gmail.com>
2016-01-30 20:26:40 -0600

    Create certs folder

--- acme-tiny-automator.sh
+++ acme-tiny-automator.sh
@@ -32,26 +32,38 @@ fi
 ACME_TINY_URL='https://raw.githubusercontent.com/diafygi/acme-tiny/master/acme_tiny.py'
 LETSENCRYPT_INTERMEDIATE_URL='https://letsencrypt.org/certs/lets-encrypt-x1-cross-signed.pem'
 LETSENCRYPT_INTERMEDIATE_CERT="$LETSENCRYPT_ROOT/intermediate.pem"
+LETSENCRYPT_ACCOUNT="$LETSENCRYPT_ROOT/account.key"
+LETSENCRYPT_CERTS="$LETSENCRYPT_ROOT/certs"
 
 # check for common tools
 ACME_TINY="$ACME_TINY_LOCAL_FOLDER/acme-tiny.py"
 GIT=$(check_component git)
+MKDIR=$(check_component mkdir)
+OPENSSL=$(check_component openssl)
 PYTHON=$(check_component python)
 WGET=$(check_component wget)
 
 # download acme-tiny, if it doesnt exist
 if [ ! -d "$
 ACME_TINY_LOCAL_FOLDER" ]; then
-    mkdir -p "$ACME_TINY_LOCAL_FOLDER"
+    $MKDIR -p "$ACME_TINY_LOCAL_FOLDER"
     $WGET $ACME_TINY_URL -O $ACME_TINY
 fi
 
-# create certificate root & intermediate, if it doesn't exit
+# create certificate folder & intermediate, if it doesn't exit
 if [ ! -f "$LETSENCRYPT_INTERMEDIATE_CERT" ]; then
     if [ ! -d "$LETSENCRYPT_ROOT" ]; then
-        mkdir -p "$LETSENCRYPT_ROOT"
+        $MKDIR -p "$LETSENCRYPT_ROOT"
     fi
     $WGET "$LETSENCRYPT_INTERMEDIATE_URL" -O $LETSENCRYPT_INTERMEDIATE_CERT
 fi
 
+# create account.key, if it doesn't exist
+if [ ! -f "$LETSENCRYPT_ACCOUNT" ]; then
+    $OPENSSL genrsa 4096 > "$LETSENCRYPT_ACCOUNT"
+fi
 
+# create certs folder, if it doesn't exist
+if [ ! -d "$LETSENCRYPT_CERTS" ]; then
+    $MKDIR -p "$LETSENCRYPT_CERTS"
+fi
