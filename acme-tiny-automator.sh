dbe178211235045db0f57b0477c036ff2b0df766
Peter Carrero <peter.carrero@gmail.com>
2016-01-30 19:32:50 -0600

    Add description to examples files

--- acme-tiny-automator.sh
+++ acme-tiny-automator.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# import settings
+# import custom settings
 # start with:
 # cp config-example.sh config.sh
 # and adjust accordingly
 . ./config.sh
+
+# general settings
+acme_tiny_url='https://raw.githubusercontent.com/diafygi/acme-tiny/master/acme_tiny.py'
---
+++ config-examples.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# this file has example settings that should work as default values for most installations
+acme_tiny_local_folder='/opt/acme-tiny'
